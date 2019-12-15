8f0b0d4d7ca501657598dfba6936ffd811e6b13c
yt1na <nkyt161@gmail.com>
2019-12-15 19:40:54 +0900

    Update local env.

--- app/main.go
+++ app/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
   e := echo.New()
   e.GET("/ping", func(c echo.Context) error {
-    return c.String(http.StatusOK, "Good bye.")
+    return c.String(http.StatusOK, "Hello nanami.")
   })
   e.Logger.Fatal(e.Start(":8080"))
 }
