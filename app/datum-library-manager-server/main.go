9243d37722b8931ecd2ef6a35c2fcbcaa777824a
yt1na <nkyt161@gmail.com>
2019-12-16 19:57:54 +0900

    Update config

--- app/datum-library-manager-server/main.go
+++ app/datum-library-manager-server/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
   e := echo.New()
   e.GET("/ping", func(c echo.Context) error {
-    return c.String(http.StatusOK, "Hello nanami.")
+    return c.String(http.StatusOK, "Hello Echo.")
   })
   e.Logger.Fatal(e.Start(":8080"))
 }
